21fe6cf6ffb0ee94e86a1683e5792a86de008c9e
testdev-fun <81535153+testdev-fun@users.noreply.github.com>
2021-03-28 14:16:24 -0700

    Create Operation.java
    Lists all the operations the calculator can perform.

---
+++ Operation.java
@@ -0,0 +1,11 @@
+public class Operation {
+   public static final String o_mul = "*";
+   public static final String o_div = "/";
+   public static final String o_add = "+";
+   public static final String o_sub = "-";
+   public static final String o_clo = ")";
+   public static final String o_opn = "(";
+   public static final String o_none = " ";
+   public static final String o_error = "-1";
+   public static final String o_end = "-2";
+}
